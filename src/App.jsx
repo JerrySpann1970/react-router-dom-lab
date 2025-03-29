9bdd0d504d7e6ddd45d8c6c5d115066bcb52e378
jerryspann <jerryspann0@gmail.com>
2025-03-28 17:20:33 -0700

    wip: working app so far

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,44 @@
-const App = () => {
-  return <h1>Hello world!</h1>;
-};
+import { useState } from "react"
+import MailboxList from "./components/MailboxList/MailboxList"
+import NavBar from './components/NavBar/Navbar'
+import MailboxDetails from "./components/MailboxDetails/MailboxDetails"
+import MailboxForm from "./components/MailboxForm/MailboxForm"
+import { Routes, Route } from "react-router"
+import "./App.css"
 
-export default App;
+const App = () => {
+  const [mailboxes, setMailboxes] = useState([])
+
+  const addBox = (newMailbox) => {
+    newMailbox._id = mailboxes.length + 1;
+    setMailboxes([...mailboxes, newMailbox]);
+  }
+
+  return (
+    <>
+      <h1>ReactVille Post Office 93637</h1>
+      <NavBar />
+      <Routes>
+        <Route
+          path="/"
+          element={<h2>Post Office</h2>} />
+        <Route
+          path="/mailboxes"
+          element={<MailboxList mailboxes={mailboxes} />}
+        />
+         <Route
+          path="/mailboxes/new"
+          element={<MailboxForm addBox={addBox} />}
+        />
+        <Route
+          path="/mailboxes/:mailboxId"
+          element={<MailboxDetails mailboxes={mailboxes} />}
+        />
+        <Route path="*" element={<h2>Whoops, nothing here!</h2>} />
+      </Routes>
+
+    </>
+  )
+}
+
+export default App
